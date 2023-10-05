8329d716a6fcfda7597c5a7c9a82901bf026969b
“javascriptseo” <seofreelance2021@outlook.com>
2023-10-04 17:19:52 -0700

    completed nested for loop

--- main.py
+++ main.py
@@ -3,16 +3,17 @@
 # Input:  None
 # Output: Prints the letter E.
 
-NUM_ACROSS = 3 # Number of asterisks to print across
-NUM_DOWN = 5 # Number of asterisks to print down
+NUM_ACROSS = 3  # Number of asterisks to print across
+NUM_DOWN = 5  # Number of asterisks to print down
 
-# Write a loop to control the number of rows.
-    # Write a loop to control the number of columns
-        # Decide when to print an asterisk in every column.
-print("*", end='')
-        # Decide when to print asterisk in column 1.
-print("*", end='')
-        # Decide when to print a space instead of an asterisk.
-print(" ", end='')
-        # Figure out where to place this statement that prints a newline.
-print("\n")
+for x in range(NUM_DOWN):  # Write a loop to control the number of rows.
+    for y in range(NUM_ACROSS):  # Write a loop to control the number of columns
+        if (x % 2 == 0):  # Decide when to print an asterisk in every column.
+            # print("*", end='')
+            print("*", end='')  # Decide when to print asterisk in column 1.
+        elif (y == 0):  # print("*", end='')
+            print("*", end='')  # Decide when to print a space instead of an asterisk.
+        else:  # print(" ", end='')
+            print(" ", end='')  # Figure out where to place this statement that prints a newline.
+
+    print("\n")
